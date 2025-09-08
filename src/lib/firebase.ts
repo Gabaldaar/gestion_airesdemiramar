d8e85faa9a562e8e4f5b98489fafe2eb5b436091
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2025-09-08 15:30:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,12 @@ import { getFirestore, Firestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // These values will be replaced by the system.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE",
-  measurementId: "YOUR_MEASUREMENT_ID_HERE"
+  "projectId": "miramar-rentals-manager",
+  "appId": "1:720916580948:web:d084f764e635bda76f3bc1",
+  "storageBucket": "miramar-rentals-manager.appspot.com",
+  "apiKey": "AIzaSyCeewCiDeeQ8CCPdAKQdx6TlpedNZnCE5Y",
+  "authDomain": "miramar-rentals-manager.firebaseapp.com",
+  "messagingSenderId": "720916580948"
 };
 
 
