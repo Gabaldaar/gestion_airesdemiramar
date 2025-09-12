13ef2fd64db24c7cbf805704a5577fd31595333d
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2025-09-12 16:11:21 +0000

    Esta es la regla que estaba. De hecho la copio y la pego y no me muestra

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,18 +14,15 @@ const firebaseConfig = {
   "messagingSenderId": "720916580948"
 };
 
-// Initialize Firebase
+// Initialize Firebase for SSR
 let app: FirebaseApp;
-let auth: Auth;
-let db: Firestore;
-
 if (getApps().length === 0) {
   app = initializeApp(firebaseConfig);
 } else {
   app = getApp();
 }
 
-auth = getAuth(app);
-db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { app, auth, db };
