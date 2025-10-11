4acb059ad5fb35eef226013c14281ed5a85a5612
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2025-10-11 15:57:31 +0000

    Bien, parece que lo logramos!!! Una cosita más que con los Ingresos. Al

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -43,6 +44,14 @@ const DialogContent = React.forwardRef<
         className
       )}
       {...props}
+      onInteractOutside={(e) => {
+        // Prevenir el cierre cuando el teclado virtual causa un resize/refocus
+        const target = e.target as HTMLElement;
+        if (target.closest('input, textarea')) {
+          e.preventDefault();
+        }
+      }}
+      data-vaul-no-dismiss
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
