1d3e78b83ee1333a6bf028f017dfd9fc28e2ddc8
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2025-10-13 15:44:09 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
         <AuthProvider>
-            <LayoutManager>{children}</LayoutManager>
+          <LayoutManager>{children}</LayoutManager>
         </AuthProvider>
       </body>
     </html>
