97e887a2bf422a11c6537ff6458848fc128cbadb
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2026-04-22 16:54:19 +0000

    Hice los pasos, me tomó el comando pero la imagen no sube

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore, Firestore, enableIndexedDbPersistence } from "firebase/firestore";
@@ -8,7 +9,7 @@ import { getStorage, FirebaseStorage } from "firebase/storage";
 const firebaseConfig = {
   "projectId": "miramar-rentals-manager",
   "appId": "1:720916580948:web:d084f764e635bda76f3bc1",
-  "storageBucket": "miramar-rentals-manager.appspot.com",
+  "storageBucket": "TU_PROYECTO.appspot.com",
   "apiKey": "AIzaSyCeewCiDeeQ8CCPdAKQdx6TlpedNZnCE5Y",
   "authDomain": "miramar-rentals-manager.firebaseapp.com",
   "messagingSenderId": "720916580948"
